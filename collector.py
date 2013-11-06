7cb99f7bec3380afbcc67efa2209508ea820d1e0
Long <long@ubuntu.ubuntu-domain>
2013-11-07 00:16:16 +0800

    Add multithreading support.

--- collector.py
+++ collector.py
@@ -6,53 +6,73 @@ import sys
 import math
 import urllib
 import urllib2
+import datetime
 import threading
 
 from bs4 import BeautifulSoup
 
 
-def update_history_data():
-    def read_codes(file_path, codes):
-        with open(file_path) as f:
+def update_history_data(thread_num=30):
+    
+    def read_codes(fp, codes):
+        with open(fp) as f:
             for line in f.readlines():
                 codes.append(line.split(' ')[0])
+
     codes = []
     read_codes('./data/code/sh', codes)
     read_codes('./data/code/sz', codes)
-    for code in codes:
-        download_history_data(code)
-    history_list = [history for history in os.listdir('./data/history') if history in codes]
-    print 'Update history data %s!\n' % 'success' if len(codes) == len(history_list) else 'failure',
-
-
-def download_history_data(code):
     
-    def download(code):
-        file_path = './data/history/' + code + '.tmp'
-        urllib.urlretrieve(url, file_path)
-        success = False
-        with open(file_path) as history:
-            lines = history.readlines()
-            if len(lines) > 0:
-                if lines[0].strip() == 'Date,Open,High,Low,Close,Volume,Adj Close':
-                    success = True
-                    os.rename(file_path, './data/history/' + code)
-            if not success:
-                os.remove(file_path)
-        return success
-    
-    success = False
-    if len(code) == 6:
-        url = 'http://table.finance.yahoo.com/table.csv?s='
-        if int(code[0]) >= 6:
-            url += code + '.ss'
+    codes_need_update = [code for code in codes]
+    for history in os.listdir('./data/history'):
+        if history in codes and datetime.date.today() == datetime.date.fromtimestamp(os.path.getmtime('./data/history/' + history)):
+            codes_need_update.remove(history)
+    print 'all=%d need_to_update=%d\n' % (len(codes), len(codes_need_update)),
+
+    workers = []
+    for i in range(0, thread_num):
+        wcodes = []
+        for j in range(i, len(codes_need_update), thread_num):
+            wcodes.append(codes_need_update[j])
+        worker = Worker(str(i), download_history_data, (wcodes,))
+        worker.start()
+        workers.append(worker)
+    for worker in workers:
+       worker.join()
+
+    history_list = []
+    for history in os.listdir('./data/history'):
+        if history in codes:
+            history_list.append(history)
         else:
-            url += code + '.sz'
-        print 'downloading %s' % url,
-        sys.stdout.flush()
-        success = download(code)
-        print 'success\n' if success else 'failure\n',
-    return success
+            os.remove('./data/history/' + history)
+    print '%s %.2f%% is updated!\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), (100 * len(history_list) / float(len(codes)))), 
+
+
+def download_history_data(codes):
+    
+    def download(url, code):
+        fp = './data/history/' + code
+        success = False
+        try:
+            csv_file = urllib2.urlopen(url, timeout=5)
+            csv_data = csv_file.read()
+            if csv_data.startswith('Date,Open,High,Low,Close,Volume,Adj Close'):
+                with open(fp, 'w') as history:
+                    history.write(csv_data)
+                success = True
+        except Exception as e:
+            print '%s\n' % str(e),
+        return success
+
+    for code in codes:
+        if len(code) == 6:
+            url = 'http://table.finance.yahoo.com/table.csv?s='
+            if int(code[0]) >= 6:
+                url += code + '.ss'
+            else:
+                url += code + '.sz'
+            print 'downloading %s %s\n' % (url, 'success' if download(url, code) else 'failure'),
 
 
 def update_holding_data():
@@ -71,16 +91,16 @@ def update_holding_data():
         for param in waiting_list:
             tid = param[0] + '_' + param[1] + '_' + str(param[2])
             params = (failure_list, param[0], param[1], param[2])
-            workers.append(Worker(tid, download_holding_data, params))
-        for worker in workers:
+            worker = Worker(tid, download_holding_data, params)
             worker.start()
+            workers.append(worker)
         for worker in workers:
            worker.join()
         if len(failure_list) > 0:
             waiting_list = failure_list
         else:
             break
-    print 'Update holding data success!\n',
+    print 'Update holding data successfully!\n',
 
 
 def download_holding_data(failure_list, category, date, quarter, page=1):
@@ -88,8 +108,8 @@ def download_holding_data(failure_list, category, date, quarter, page=1):
     url = 'http://vip.stock.finance.sina.com.cn/q/go.php/vComStockHold/kind/' + category + '/index.phtml?reportdate=' + date + '&quarter=' + str(quarter) + '&p=' + str(page)
     print 'downloading %s\n' % url,
     try:
-        html = urllib2.urlopen(url).read().decode('GB2312')
-        soup = BeautifulSoup(html)
+        html = urllib2.urlopen(url, timeout=5)
+        soup = BeautifulSoup(html.read().decode('GB2312'))
         table = soup.find(id='dataTable')
         for tr in table.find_all('tr', recursive=False):
             if not tr.has_attr('style'):
@@ -97,7 +117,8 @@ def download_holding_data(failure_list, category, date, quarter, page=1):
                 holding_data_list.append(holding_data)
         if len(holding_data_list) > 0 and soup.find('div', class_='pages').find('a', class_='page nolink', text=u'下一页') is None:
             holding_data_list += download_holding_data(failure_list, category, date, quarter, page + 1)
-        if page == 1:
+        if page == 1 and (category, date, quarter) not in failure_list:
+            print 'updating %s_%s_%d size=%d\n' % (category, date, quarter, len(holding_data_list)),
             with open('./data/' + category + '/' + date + '_' + str(quarter), 'w') as f:
                 for holding_data in holding_data_list:
                     f.write(','.join(holding_data).encode('utf-8') + '\n')
@@ -116,20 +137,20 @@ def check_holding_data():
     for category in categories:
         for date in dates:
             for quarter in date_quarter_dict[date]:
-                file_path = './data/' + category + '/' + date + '_' + str(quarter)
-                with open(file_path) as f:
+                fp = './data/' + category + '/' + date + '_' + str(quarter)
+                with open(fp) as f:
                     total = len(f.readlines())
                     page = int(math.ceil(total / 40.0))
                     url = 'http://vip.stock.finance.sina.com.cn/q/go.php/vComStockHold/kind/' + category + '/index.phtml?reportdate=' + date + '&quarter=' + str(quarter) + '&p=' + str(page)
-                    html = urllib2.urlopen(url).read().decode('GB2312')
-                    soup = BeautifulSoup(html)
+                    html = urllib2.urlopen(url, timeout=5)
                     check_ok = False
+                    soup = BeautifulSoup(html.read().decode('GB2312'))
                     if soup.find('div', class_='pages').find('a', class_='page nolink', text=u'下一页') is not None:
                         check_ok = True
                     else:
                         all_data_ok = False
-                    print 'checking %s %d %d %s\n' % (file_path, total, page, 'success' if check_ok else 'failure'),
-    print 'The data is %s\n' % 'complete' if all_data_ok else 'not complete',
+                    print 'checking %s %d %d %s\n' % (fp, total, page, 'success' if check_ok else 'failure'),
+    print 'The data is %s!\n' % 'complete' if all_data_ok else 'not complete',
 
 
 class Worker(threading.Thread):
@@ -146,3 +167,5 @@ class Worker(threading.Thread):
 
 if __name__ == '__main__':
     update_history_data()
+    # update_holding_data()
+    # check_holding_data()
